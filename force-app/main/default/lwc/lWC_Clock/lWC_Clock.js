ca564b7394b1622da3cd06ed7943dfda9da6c09c
Aritra Mukherjee <aritra@gram-aritra>
2020-08-22 21:25:04 +0530

    Clock Version 1

--- force-app/main/default/lwc/lWC_Clock/lWC_Clock.js
+++ force-app/main/default/lwc/lWC_Clock/lWC_Clock.js
@@ -1,39 +1,50 @@
+/* eslint-disable @lwc/lwc/no-async-operation */
 import { LightningElement } from "lwc";
 
 export default class App extends LightningElement {
   showTime = true;
+  running = false;
+  clockId;
   timeElapsed = 0;
-  resetInfo = [
+  clockInfo = [
     {
       time: Date.now(),
       name: "Aritra"
     }
   ];
   //@track resetInfo={}
+  processId = 0;
 
   constructor() {
     super();
-    setInterval(() => {
-      this.timeElapsed = this.timeElapsed + 1;
-      //console.log(`Time elapsed ${this.timeElapsed} seconds`);
-    }, 1000);
+    this.startClock();
   }
 
   resetClock(e) {
-    this.timeElapsed = 0;
-    this.resetInfo = []; //temp done
-    this.resetInfo.push({
-      time: Date.now(),
-      name: "Aritra"
-    });
-    console.log(JSON.stringify(this.resetInfo));
+    this.stopClock();
+    this.startClock();
   }
 
   stopClock(e) {
-    //clearInterval();
+    clearInterval(this.clockId);
+    this.timeElapsed = 0;
+    this.running = false;
   }
 
   startClock(e) {
-    this.resetClock();
+    this.clockInfo = []; //temp done
+    this.clockInfo.push({
+      time: Date.now(),
+      name: "Aritra"
+    });
+    console.log(JSON.stringify(this.clockInfo));
+    if (!this.running) {
+      this.clockId = setInterval(() => {
+        this.running = true;
+        this.timeElapsed++;
+      }, 1000);
+    }
+
+    //console.log('Current clock Id : ' + this.clockId);
   }
 }
